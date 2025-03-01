dfd8f9137111a3d7967238fd83afaa24a3324315
nureazambd <nur.azam.bd@gmail.com>
2025-03-01 19:08:04 +0600

    git page update

---
+++ src/components/Achievements.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Achievements() {
+  return (
+    <div className="container px-5 py-12 mx-auto max-w-7xl">
+      <div className="flex flex-col text-left w-full mb-2">
+    <h1 className="lg:w-2/3 uppercase leading-relaxed text-[18px] font-semibold text-[#aaa6c3]">achievements</h1>
+      <h1 className="sm:text-6xl text-3xl font-black title-font mb-2 text-gray-50">Achievements.</h1>
+    </div>
+
+    <div>
+        <div>
+        <h3 className='text-white text-[24px] font-bold'>Licenses and Certifications</h3>
+        
+        <ul className='mt-5 list-disc ml-10 space-y-2'>
+          <li className='text-white-100 text-[16px] pl-1 tracking-wider'>
+          Python (Basic) Certificate, Md. Nur E Azam's HackerRank Certificates <a href="https://www.hackerrank.com/certificates/30ea0a3a764b" target="_blank" rel="noreferrer"> [see credential]</a>
+          </li>
+
+          <li className='text-white-100 text-[16px] pl-1 tracking-wider'>
+          JavaScript Basics University of California, Davis Certificates<a href="https://www.coursera.org/account/accomplishments/verify/WRWLLXAYFJWY" target="_blank" rel="noreferrer"> [see credential]</a>
+          </li>
+
+          <li className='text-white-100 text-[16px] pl-1 tracking-wider'>
+          Modern JavaScript: ES6 Basics<a href="https://coursera.org/share/ac88ab0b75a29fff893549aab71b9c5b" target="_blank" rel="noreferrer"> [see credential]</a>
+          </li>
+
+          <li className='text-white-100 text-[16px] pl-1 tracking-wider'>
+          Introduction to Artificial Intelligence (AI)<a href="https://www.coursera.org/account/accomplishments/verify/MQEEB2F2WCXU" target="_blank" rel="noreferrer"> [see credential]</a>
+          </li>
+
+          <li className='text-white-100 text-[16px] pl-1 tracking-wider'>
+          Python (Basic) Certificate<a href="https://www.hackerrank.com/certificates/30ea0a3a764b" target="_blank" rel="noreferrer"> [see credential]</a>
+          </li>
+
+          <li className='text-white-100 text-[16px] pl-1 tracking-wider'>
+          Data Engineering for Everyone, authorized by Hadrien Lacroix and offered through DataCamp <a href="https://www.datacamp.com/statement-of-accomplishment/course/75b7ebd89df8c2509bae613331a193e01672b996" target="_blank" rel="noreferrer">[see credential]</a>
+          </li>
+          
+
+      </ul>
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Achievements
